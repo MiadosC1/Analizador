4003cff6a933bbfbe8b9f7a4b761f37cffc4faa3
Ricardo Ibarra <116777631+MiadosC1@users.noreply.github.com>
2025-05-08 23:37:46 -0700

    Add files via upload

---
+++ analizadorlexicocopia/src/codigo/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package codigo;
+
+import java.io.File;
+
+/**
+ *
+ * @author 22211585
+ */
+public class Principal {
+    public static void main(String[] args) 
+    {
+        String ruta = "C:/Users/Estudio/Desktop/analizadorlexicocopia/src/codigo/Lexer.flex";
+        generarLexer(ruta);
+    }
+    public static void generarLexer(String ruta)
+    {
+        File archivo = new File(ruta);
+        JFlex.Main.generate(archivo);
+    }
+}
